d491cad611e0cc382f23e3976afce1226b64294f
Rodrigo Queiroz <rodrigo.queiroz0629@gmail.com>
2026-05-31 11:28:03 -0300

    feat(api): adiciona limite de requisições às rotas de astrologia

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,5 +3,7 @@
 use App\Http\Controllers\AstrologyController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('astrology/analyze', [AstrologyController::class, 'analyze']);
-Route::post('astrology/babel-fish', [AstrologyController::class, 'babelFish']);
+Route::middleware('throttle:5,1')->group(function () {
+    Route::post('astrology/analyze', [AstrologyController::class, 'analyze']);
+    Route::post('astrology/babel-fish', [AstrologyController::class, 'babelFish']);
+});
